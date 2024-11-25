1b8a1a4be9848c84eb192c4ceff152a4376f2858
borisnikolov <sttm.devs@outlook.com>
2024-11-24 20:14:58 -0500

    Desktop UI
    Added DesktopScene

---
+++ Assets/Environment/UI/Task.cs
@@ -0,0 +1,11 @@
+using Unity.Netcode;
+using UnityEngine;
+
+[System.Serializable]
+public class Task
+{
+    public string Title;
+    public string Category;
+    public string Description;
+    public bool IsCompleted;
+}
